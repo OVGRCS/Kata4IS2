19a8b56db950f879b8532c135ba321f86f204c60
usuario <usuario@10.22.143.8>
2015-11-04 10:32:01 +0000

    tercera versión

--- src/kata4/KATA4.java
+++ src/kata4/KATA4.java
@@ -1,65 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata4;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
+
 
 /**
  *
- * @author usuario
+ * @author Omaro Vega
  */
 public class KATA4 {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        String name = "C:\\Users\\Usuario\\Documents";
-        File file = new File (name);
+    public static void main(String[] args) throws IOException{
         
-        String[] filearray = file.list();
+        String name = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\KATA4\\DATA\\emails.txt";
         
-        /*for (String files : filearray){
-            System.out.println(files);    
-        }
-        print(file.listFiles(),"");*/
-        
-        String from = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Jellyfish.jpg";
-        String to = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Jellyfish(1).jpg";
-        
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
-        
-        byte [] buffer = new byte[1024];
-        
-        Date date = new Date();
-        while(true){
-            int read = input.read(buffer);
-            if(read<0) break;
-            output.write(buffer);
-        }
-        System.out.println(new Date().getTime()-date.getTime() + "ms");
-        input.close();
-        output.flush();
-        output.close();
+        ArrayList<String> listMail = MailList.read(name);
+        Histogram<String> histogram = MainHistogramBuilder.build(listMail);
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        histogramDisplay.execute();
     }
-
-    private static void print(File[] listFiles, String ident) {
-        if(listFiles == null) return;
-        for (File listFile : listFiles) {
-            System.out.println(ident+(listFile.isDirectory()? "+":" ")+listFile.getName());
-            if(listFile.isFile() || listFile.isHidden())continue;
-            print(listFile.listFiles(),ident + " ");
-        }
-
-    }
-
-    
 }
---
+++ src/kata4/MainHistogramBuilder.java
@@ -0,0 +1,16 @@
+package kata4;
+
+import java.util.ArrayList;
+
+public class MainHistogramBuilder {
+    
+    public static Histogram<String> build (ArrayList<String> mailList){
+        Histogram<String> histo = new Histogram<>();
+        
+        for (String mail : mailList) {
+            histo.increment(new Mail(mail).getDominio());
+        }
+        return histo;
+    }
+    
+}
